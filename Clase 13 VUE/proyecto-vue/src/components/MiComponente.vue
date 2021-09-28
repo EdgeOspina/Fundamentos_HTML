5f82f1ca37661a5aed02cfe866e24771cb868838
Edgar Ospina <e-ospina@hotmail.com>
2021-09-28 15:49:46 -0500

    update

---
+++ Clase 13 VUE/proyecto-vue/src/components/MiComponente.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1> {{Titulo}}</h1>
+        <h2> {{Subtitulo}}</h2>
+        <input v-model="numero1" type="number" placeholder="Numero 1" />
+        <br>
+        <input v-model="numero2" type="number" placeholder="Numero 2" />
+        <br>
+        <button v-on:click="sumar">Sumar</button>
+        <br>
+        <span>Resultado: {{resultado}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    //Data contiene las variables globales del componente
+    data: ()=>{
+        return{
+            Titulo: 'Titulo Componente',
+            Subtitulo: 'Subtítulo Componente',
+            numero1: null,
+            numero2: null,
+            resultado: '',
+        }
+    },
+    methods:{
+        sumar(){
+            this.resultado = parseInt(this.numero1) + parseInt(this.numero2);
+    }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
